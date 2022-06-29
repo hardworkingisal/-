876f70fa535778564789091a44412d9a692c8989
hardworkingisal <108267592+hardworkingisal@users.noreply.github.com>
2022-06-29 18:27:41 +0800

    1
    第一天

---
+++ Project2/test.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+//int main()
+//{
+//	printf("杜泽宁\n");
+//	return 0;
+//}
+
+
+
+
+
+
+int main()
+{
+	printf("我最强\n");
+	printf("夸克\n");
+	printf("ch\n");
+	char ch = 'a';
+	printf("ch\n");
+	int length = 2147483648;
+	short length1 = 32768;
+	long month = 10101010101010101039;
+	printf("%d\n", 130);
+	printf("%d\n", sizeof(char));
+	printf("%d\n", sizeof(long));
+	printf("%d\n", sizeof(int));
+	printf("%d\n", sizeof(float));
+}
